44c22ba6b268aab02dfd4ffc4d217111142c5ae8
tusharrajput001 <devoptushar@gmail.com>
2024-05-05 21:24:39 +0530

    navbar responsiveness

--- Frontend/src/components/Navbar/navbar.jsx
+++ Frontend/src/components/Navbar/navbar.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import './navbar.css'; // Import CSS file for styling if needed
+import React, { useState } from 'react';
+import './navbar.css';
+import '../../fonts/fonts.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <span className="navbar-heading">Sneazers</span>
       </div>
-      <div className="navbar-options">
+
+      {/* Hamburger Icon */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
+      </div>
+
+      {/* Navigation Options */}
+      <div className={`navbar-options ${isOpen ? 'open' : ''}`}>
         <ul>
           <li><a href="#">High-top</a></li>
           <li><a href="#">Mid-top</a></li>
@@ -15,11 +29,13 @@ function Navbar() {
           <li><a href="#">Slip-ons</a></li>
         </ul>
       </div>
+
+      {/* Icons */}
       <div className="navbar-icons">
         <ul>
-          <li><a href="#"><i className="fas fa-search"></i></a></li>
-          <li><a href="#"><i className="fas fa-user"></i></a></li>
-          <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+          <li><a href="#"><i className="fas fa-search black-icon"></i></a></li>
+          <li><a href="#"><i className="fas fa-user black-icon"></i></a></li>
+          <li><a href="#"><i className="fas fa-shopping-cart black-icon"></i></a></li>
         </ul>
       </div>
     </nav>
