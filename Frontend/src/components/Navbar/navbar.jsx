2b48875507a007f20f18c6bd53f95a1a63356951
tusharrajput001 <devoptushar@gmail.com>
2024-05-05 22:07:42 +0530

    search-bar

--- Frontend/src/components/Navbar/navbar.jsx
+++ Frontend/src/components/Navbar/navbar.jsx
@@ -1,44 +1,85 @@
-import React, { useState } from 'react';
-import './navbar.css';
-import '../../fonts/fonts.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import "../../fonts/fonts.css";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // state for hamburger menu
+  const [isSearchOpen, setIsSearchOpen] = useState(false); // State for search bar visibility
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <span className="navbar-heading">Sneazers</span>
-      </div>
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-heading">
+            Sneazers
+          </Link>
+        </div>
 
-      {/* Hamburger Icon */}
-      <div className="hamburger-menu" onClick={toggleMenu}>
-        <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
-      </div>
+        {/* Hamburger Icon */}
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <i className={`fas ${isOpen ? "fa-times" : "fa-bars"}`}></i>
+        </div>
 
-      {/* Navigation Options */}
-      <div className={`navbar-options ${isOpen ? 'open' : ''}`}>
-        <ul>
-          <li><a href="#">High-top</a></li>
-          <li><a href="#">Mid-top</a></li>
-          <li><a href="#">Low-top</a></li>
-          <li><a href="#">Slip-ons</a></li>
-        </ul>
-      </div>
+        {/* Navigation Options */}
+        <div className={`navbar-options ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <a href="#">High-top</a>
+            </li>
+            <li>
+              <a href="#">Mid-top</a>
+            </li>
+            <li>
+              <a href="#">Low-top</a>
+            </li>
+            <li>
+              <a href="#">Slip-ons</a>
+            </li>
+          </ul>
+        </div>
 
-      {/* Icons */}
-      <div className="navbar-icons">
-        <ul>
-          <li><a href="#"><i className="fas fa-search black-icon"></i></a></li>
-          <li><a href="#"><i className="fas fa-user black-icon"></i></a></li>
-          <li><a href="#"><i className="fas fa-shopping-cart black-icon"></i></a></li>
-        </ul>
+        {/* Icons */}
+        <div className="navbar-icons">
+          <ul>
+            {/* Search Icon */}
+            <li>
+              <a href="#" onClick={toggleSearch}>
+                <i className="fas fa-search black-icon"></i>
+              </a>
+            </li>
+            {/* User Icon */}
+            <li>
+              <Link to="/login">
+                <i className="fas fa-user black-icon"></i>
+              </Link>
+            </li>
+            {/* Cart Icon */}
+            <li>
+              <a href="#">
+                <i className="fas fa-shopping-cart black-icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {/* Search Bar */}
+      <div className={`search-bar ${isSearchOpen ? 'active' : ''}`}>
+        <input type="text" placeholder="Search..." />
+        {/* Close Button */}
+        <button className="close-btn" onClick={toggleSearch}>
+          <i className="fas fa-times"></i>
+        </button>
       </div>
-    </nav>
+    </>
   );
 }
 
