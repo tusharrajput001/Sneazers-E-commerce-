e1a84d507f18467f2e2bf72ded248d27877cf429
tusharrajput001 <devoptushar@gmail.com>
2024-05-07 14:02:01 +0530

    try catch in asynawait

--- Backend/Routes/usersRoutes.js
+++ Backend/Routes/usersRoutes.js
@@ -5,41 +5,46 @@ const RegisterModel = require("../Models/Register.model");
 
 const app = express();
 app.use(cors());
-dotenv.config();  
+dotenv.config();
 app.use(express.json());
 
-
 // registration data
-app.post("/",  async(req, res) => {
-  await RegisterModel.create(req.body)
-    .then((registers) => res.json(registers))
-    .catch((err) => res.json(err));
+app.post("/", async (req, res) => {
+  try {
+    const register = await RegisterModel.create(req.body);
+    res.json(register);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Route to fetch all data
-app.get("/", async(req, res) => {
-  await RegisterModel.find({})
-    .then((registers) => res.json(registers))
-    .catch((err) => res.json(err));
+app.get("/", async (req, res) => {
+  try {
+    const registers = await RegisterModel.find({});
+    res.json(registers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-// login 
-app.post("/login", async(req, res) => {
+// login
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  await RegisterModel.findOne({ email: email }).then((user) => {
+  try {
+    const user = await RegisterModel.findOne({ email: email });
     if (user) {
       if (user.password === password) {
         res.json("Success");
       } else {
         res.json("Failed");
-      } 
+      }
     } else {
       res.json("Not registered");
     }
-  });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-
-
-
 
 module.exports = app;
