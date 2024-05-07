80fea446fe6d1312204c3e403fedd7ca42261210
tusharrajput001 <devoptushar@gmail.com>
2024-05-07 17:43:59 +0530

    render

--- Frontend/src/components/Login/login.jsx
+++ Frontend/src/components/Login/login.jsx
@@ -19,7 +19,7 @@ function Login() {
       return;
     }
     axios
-      .post("https://sneazers-e-commerce-g74gu8xg1-tusharrajput001s-projects.vercel.app/login", { email, password })
+      .post("https://sneazers-e-commerce.onrender.com/login", { email, password })
       .then((result) => {
         console.log(result);
         if (result.data === "Success") {
