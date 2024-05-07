4da18c890339db5e5e703e5053d5b63451480aff
tusharrajput001 <devoptushar@gmail.com>
2024-05-07 13:42:20 +0530

    app.js change

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,30 @@
-  const express = require("express");
-  const mongoose = require("mongoose");
-  const cors = require("cors");
-  const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-  const userRoute = require("./Routes/usersRoutes");
-  const app = express();
-  app.use(cors());
-  dotenv.config();
-  app.use(express.json());
-  const DB = "mongodb+srv://devoptushar:sneazerspass@cluster0.xqxgo0w.mongodb.net/sneazers?retryWrites=true&w=majority&appName=Cluster0";
+const userRoute = require("./Routes/usersRoutes");
+const app = express();
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+const DB = "mongodb+srv://devoptushar:sneazerspass@cluster0.xqxgo0w.mongodb.net/sneazers?retryWrites=true&w=majority&appName=Cluster0";
 
-  mongoose
-    .connect(DB, {})
-    .then(() => {
-      console.log("connected successfully");
-      app.listen(process.env.PORT || 8000, (err) => {
-        if (err) console.log(err);
-        console.log("running successfully at", process.env.PORT);
-      });
-    })
+mongoose
+  .connect(DB, {})
+  .then(() => {
+    console.log("connected successfully");
+    app.listen(process.env.PORT || 8000, (err) => {
+      if (err) {
+        console.log(err);
+        process.exit(1); // Exiting the process if unable to listen on port
+      }
+      console.log("running successfully at", process.env.PORT || 8000);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1); // Exiting the process if unable to connect to the database
+  });
 
-    .catch((err) => console.log(err));
-
-  app.use(userRoute);
+app.use(userRoute);
