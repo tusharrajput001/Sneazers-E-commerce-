8b3a3b4d367f2db67b4cccda57102bb8e8415832
tusharrajput001 <devoptushar@gmail.com>
2024-06-09 23:57:23 +0530

    dash

--- Frontend/src/components/Pages/Allproducts/allproducts.jsx
+++ Frontend/src/components/Pages/Allproducts/allproducts.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ProductCard from '../../ProductCard/productCard';
+import './allproducts.css'; // Import the CSS file
 
 function Allproducts({ products }) {
   return (
     <>
+    
       {products.length > 0 ? (
         <div className="product-list">
           {products.map((product, index) => (
-            <ProductCard
-              key={index}
-              image={product.image}
-              brand={product.brand}
-              name={product.name}
-              price={product.price}
-            />
+            <div key={index} className="product-card"> {/* Wrap ProductCard in a div with the product-card class */}
+              <ProductCard
+                image={product.image}
+                brand={product.brand}
+                name={product.name}
+                price={product.price} 
+              />
+            </div>
           ))}
         </div>
       ) : (
