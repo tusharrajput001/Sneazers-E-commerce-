ce7a608402a7130fc8d161fc7c5912dc080cb2ee
tusharrajput001 <devoptushar@gmail.com>
2024-06-11 17:11:17 +0530

    latest products

--- Frontend/src/components/LatestProducts/LatestProducts.jsx
+++ Frontend/src/components/LatestProducts/LatestProducts.jsx
@@ -4,20 +4,14 @@ import './LatestProducts.css';
 import { Link } from 'react-router-dom';
 
 function LatestProducts({ products }) {
-    // Separate products with and without orderAddedDate
+    // Filter products with orderAddedDate
     const productsWithDate = products.filter(product => product.orderAddedDate);
-    const productsWithoutDate = products.filter(product => !product.orderAddedDate);
 
-    // Sort products with orderAddedDate by descending order
-    const sortedProducts = productsWithDate
-      .slice()
-      .sort((a, b) => new Date(b.orderAddedDate) - new Date(a.orderAddedDate));
+    // Sort products by orderAddedDate in descending order
+    const sortedProducts = productsWithDate.sort((a, b) => new Date(b.orderAddedDate) - new Date(a.orderAddedDate));
 
-    // Take the first 8 sorted products with orderAddedDate
+    // Take the first 8 sorted products
     const displayedProducts = sortedProducts.slice(0, 8);
-
-    // Concatenate products without orderAddedDate after the sorted ones
-    const allDisplayedProducts = [...displayedProducts, ...productsWithoutDate];
 
     return (
       <section className="LatestProducts">
@@ -25,19 +19,21 @@ function LatestProducts({ products }) {
           <h2 className="section-title">Latest Products</h2>
           <Link style={{display:'flex', justifyContent:"end", marginRight:'20px'}} to='/allproducts'>View all</Link>
           <div className="product-grid">
-            {allDisplayedProducts.map((product, index) => ( // Corrected mapping here
-              <div key={index} className="product-card">
-                    <ProductCard
-                      key={product._id}
-                      id={product._id}
-                      image={product.image}
-                      brand={product.brand}
-                      name={product.name}
-                      price={product.price}
-                    />
+            {displayedProducts.map(product => (
+              <div key={product._id} className="product-card">
+                <ProductCard
+                  id={product._id}
+                  image={product.image}
+                  brand={product.brand}
+                  name={product.name}
+                  price={product.price}
+                />
               </div>
             ))}
           </div>
+          {productsWithDate.length > 8 && (
+            <button onClick={() => {}}>View More</button>
+          )}
         </div>
       </section>
     );
