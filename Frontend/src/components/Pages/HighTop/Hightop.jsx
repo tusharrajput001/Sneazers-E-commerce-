6f360d0473c1023783d9bb2feb9b6b74c4f35055
tusharrajput001 <devoptushar@gmail.com>
2024-06-11 15:23:59 +0530

    hightop css fixed

--- Frontend/src/components/Pages/HighTop/Hightop.jsx
+++ Frontend/src/components/Pages/HighTop/Hightop.jsx
@@ -2,40 +2,35 @@ import React from 'react';
 import ProductCard from '../../ProductCard/productCard';
 import './Hightop.css';
 
-
 function HighTop({ products }) {
   // Filter products by category "High-top"
   const highTopProducts = products.filter(product => product.category === "High-top");
 
   return (
-    <section className="HPageProducts">
-      <div className="HPageContainer">
-        <div className="HtopContainer">
-          <div className="heading-container">
-            <h1>
-              High Top Products
-            </h1>
-          </div>
-          <div className="products-container">
-            {highTopProducts.length > 0 ? (
-              <div className="product-list">
-                {highTopProducts.map((product, index) => (
-                  <div key={index} className="product-card">
-                    <ProductCard
-                      key={product._id}
-                      id={product._id}
-                      image={product.image}
-                      brand={product.brand}
-                      name={product.name}
-                      price={product.price}
-                    />
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p>No high-top products available</p>
-            )}
-          </div>
+    <section className="high-top-products">
+      <div className="high-top-container">
+        <div className="high-top-header">
+          <h1>High Top Products</h1>
+        </div>
+        <div className="high-top-list">
+          {highTopProducts.length > 0 ? (
+            <div className="high-top-items">
+              {highTopProducts.map((product, index) => (
+                <div key={index} className="high-top-item">
+                  <ProductCard
+                    key={product._id}
+                    id={product._id}
+                    image={product.image}
+                    brand={product.brand}
+                    name={product.name}
+                    price={product.price}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>No high-top products available</p>
+          )}
         </div>
       </div>
     </section>
