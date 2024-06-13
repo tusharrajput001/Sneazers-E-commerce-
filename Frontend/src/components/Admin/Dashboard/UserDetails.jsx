7360f49d98828a372e56a99de4a723740220fff3
tusharrajput001 <devoptushar@gmail.com>
2024-06-13 14:02:21 +0530

    user Details

--- Frontend/src/components/Admin/Dashboard/UserDetails.jsx
+++ Frontend/src/components/Admin/Dashboard/UserDetails.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-// import './Users.css';
+import React, { useState, useEffect } from 'react';
+import './UserDetails.css';
 
 function UserDetails() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => { 
+    fetch('http://localhost:3000/')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
   return (
-    <div>
+    <div className='usersTable'>
       <h2>Users List</h2>
-      {/* Add your users list here */}
+      <table className="users-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user._id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
